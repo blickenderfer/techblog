300320ac15cc41f255eaf24da743faa8856d8a37
Blickenderfer <ecblickenderfer@gmail.com>
2023-06-19 17:33:03 -0400

    began layouts, finished main handlebars file

--- models/Comment.js
+++ models/Comment.js
@@ -15,7 +15,7 @@ Comment.init({
     date_created: {
         type: DataTypes.DATE,
         allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        defaultValue: DataTypes.NOW
     },
     blog_id: {
         type: DataTypes.INTEGER,
