045677af0fc42ccd3e7bf54ca2c1f0f60820161b
Blickenderfer <ecblickenderfer@gmail.com>
2023-06-27 18:11:26 -0400

    fixed port number

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const hb = exphb.create({helpers});
 const sesh = {
     secret: "top_secret",
